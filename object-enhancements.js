c1dddeb0bcb36d8ab440eddcc3d8aa0c09200231
Juliet Stunkel <rstunkel97@gmail.com>
2023-03-13 11:29:59 -0500

    objects have been enhanced

---
+++ object-enhancements.js
@@ -0,0 +1,68 @@
+// In this exercise, you’ll refactor some ES5 code into ES2015. Write your code in the sections with a comment to “Write an ES2015 Version”.
+// Same Keys and Values
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//  }
+// ES2015 version
+function createInstructor(firstName, lastName){
+    return {
+        firstName,
+        lastName
+    };
+}
+
+// Computed Property Names
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+//ES2015 version
+let favoriteNumber = 42;
+
+const instructor = {
+    firstName : 'Colt',
+    [favoriteNumber] : 'That is my favorite!'
+};
+
+// Object Methods
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi: function(){
+//       return "Hi!";
+//     },
+//     sayBye: function(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+//ES2015 version
+const instructor1 = {
+    firstName : 'Colt',
+    sayHi(){
+        return 'Hi!';
+    },
+    sayBye(){
+        return this.firstName + 'says bye!';
+    }
+};
+
+// Create Animal Function
+// Write a function which generates an animal object. The function should accepts 3 arguments:
+// - species: the species of animal (‘cat’, ‘dog’)
+// - verb: a string used to name a function (‘bark’, ‘bleet’)
+// - noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+// Use one or more of the object enhancements we’ve covered.
+function createAnimal(species, verb, noise){
+    const newAnimal = {
+        species,
+        [verb](){
+            return this.species + ' goes ' + noise;
+        }
+    };
+    return newAnimal[verb]();
+}
